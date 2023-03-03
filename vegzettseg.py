28fec88c1895708e80273b05b7a868ec59d76332
I_Benedek <ivankovicsbenedek.otthoni@gmail.com>
2023-03-03 19:52:03 +0100

    commit 5

---
+++ vegzettseg.py
@@ -0,0 +1,37 @@
+from collections import Counter
+
+
+def bejaras_szetvalasztas(nagy_lista, foglalkozasok):
+    for sor in nagy_lista:
+        darabolt = sor.strip().split(',')
+        foglalkozasok.append(darabolt[9])
+        foglalkozasok.append(darabolt[10])
+    for index, elem in enumerate(foglalkozasok):
+        if elem == '' or elem == ' ':
+            del foglalkozasok[index]
+
+
+def eldontes_kiiras(foglalkozasok):
+    # Három leggyakoribb foglalkozás
+    gyakorisag = Counter(foglalkozasok)
+    leggyakoribb = gyakorisag.most_common(3)
+    print('Három leggyakoribb foglalkozás: ')
+    print(leggyakoribb[0][0] + ',', leggyakoribb[1][0] + ',', leggyakoribb[2][0] + ',')
+    print('----------------------------------------------------')
+    # Három legritkább foglalkozás
+    legritkabb = gyakorisag.most_common()[-3:]
+    print('Három legritkább foglalkozás: ')
+    print(legritkabb[0][0] + ',', legritkabb[1][0] + ',', legritkabb[2][0] + ',')
+
+
+def main():
+    print('A program kiírja a három leggyakoribb és a három legritkább foglalkozás ezen űrhajósok körében')
+    print('----------------------------------------------------------------------')
+    with open('astronauts.csv') as forrasfajl:
+        nagy_lista = forrasfajl.readlines()
+    foglalkozasok = []
+    bejaras_szetvalasztas(nagy_lista, foglalkozasok)
+    eldontes_kiiras(foglalkozasok)
+
+
+main()
