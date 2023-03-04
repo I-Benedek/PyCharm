f51ac46daa24ecf2b95e785d2a9a0b0c6024e371
I_Benedek <ivankovicsbenedek.otthoni@gmail.com>
2023-03-04 20:58:59 +0100

    Javított feladat

--- vegzettseg.py
+++ vegzettseg.py
@@ -1,37 +1,35 @@
 from collections import Counter
 
 
-def bejaras_szetvalasztas(nagy_lista, foglalkozasok):
-    for sor in nagy_lista:
-        darabolt = sor.strip().split(',')
-        foglalkozasok.append(darabolt[9])
-        foglalkozasok.append(darabolt[10])
-    for index, elem in enumerate(foglalkozasok):
-        if elem == '' or elem == ' ':
-            del foglalkozasok[index]
-
-
-def eldontes_kiiras(foglalkozasok):
-    # Három leggyakoribb foglalkozás
-    gyakorisag = Counter(foglalkozasok)
+def gyakori(honapok):
+    gyakorisag = Counter(honapok)
     leggyakoribb = gyakorisag.most_common(3)
-    print('Három leggyakoribb foglalkozás: ')
-    print(leggyakoribb[0][0], str(round(leggyakoribb[0][1]/len(foglalkozasok)*100, 1))+'%,', leggyakoribb[1][0], str(round(leggyakoribb[1][1]/len(foglalkozasok)*100, 1))+'%,', leggyakoribb[2][0], str(round(leggyakoribb[2][1]/len(foglalkozasok)*100, 1)))
+    return leggyakoribb
+
+
+def kiiras(leggyakoribb, honapok):
+    print('A három leggyakoribb születési hónap fordított sorrendben: ')
+    print(leggyakoribb[2][0] + '.hó-', str(round(leggyakoribb[2][1] / len(honapok) * 100, 1)) + '%;  ',
+          leggyakoribb[1][0] + '.hó-',
+          str(round(leggyakoribb[1][1] / len(honapok) * 100, 1)) + '%;  ', leggyakoribb[0][0] + '.hó-',
+          str(round(leggyakoribb[0][1] / len(honapok) * 100, 1)) + '%;  ')
     print('----------------------------------------------------')
-    # Három legritkább foglalkozás
-    legritkabb = gyakorisag.most_common()[-3:]
-    print('Három legritkább foglalkozás: ')
-    print(legritkabb[0][0], str(round(legritkabb[0][1]/len(foglalkozasok)*100, 1))+'%,', legritkabb[1][0], str(round(legritkabb[1][1]/len(foglalkozasok)*100, 1))+'%,',legritkabb[2][0], str(round(legritkabb[2][1]/len(foglalkozasok)*100, 1)))
 
 
 def main():
-    print('A program kiírja a három leggyakoribb és a három legritkább foglalkozás ezen űrhajósok körében')
+    print('Melyik a három leggyakoribb születési hónap ezen űrhajósok körében?')
     print('----------------------------------------------------------------------')
+    honapok = []
     with open('astronauts.csv') as forrasfajl:
         nagy_lista = forrasfajl.readlines()
-    foglalkozasok = []
-    bejaras_szetvalasztas(nagy_lista, foglalkozasok)
-    eldontes_kiiras(foglalkozasok)
+    for sor in nagy_lista:
+        darabolt = sor.strip().split(',')
+        egybe_ido = darabolt[4]
+        ido_darabolva = egybe_ido.strip().split('/')
+        honap = ido_darabolva[0]
+        honapok.append(honap)
+    leggyakoribb = gyakori(honapok)
+    kiiras(leggyakoribb, honapok)
 
 
 main()
